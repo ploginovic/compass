6ea66def75d90e0edb14b3992d9263d922dc45cc
ploginovic <pl450@exeter.ac.uk>
2024-11-20 22:31:38 +0000

    Empty page, just an idea

---
+++ frontend/src/pages/CompetitionRatios.js
@@ -0,0 +1,5 @@
+// Empty page for now, just an idea
+// Collating all competition ratios across the years is an easy win 
+// And will be a useful resource to have on our website for people to explore
+// Almost like a thing people refer to MedMap for
+// But super easy to do
